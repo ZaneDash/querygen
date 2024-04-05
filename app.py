070af047f719c869b27893c392681aace06823ce
Zane S. Dash <zanedash@college.harvard.edu>
2024-04-04 17:13:53 -0700

    final edits

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Load your model
+# Load the model
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 pretrained_bart = BartForConditionalGeneration.from_pretrained('facebook/bart-base')
 bart_model = BART(tokenizer, pretrained_bart)
-bart_model.load_state_dict(torch.load('bart_best_model.pth', map_location=torch.device('cpu')))
+state_dict = torch.load('bart_best_model.pth', map_location=torch.device('cpu'))
+bart_model.load_state_dict(state_dict)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     input_text = data['text']
-    # You might need to adjust the following line according to your model's method for prediction
     output = bart_model.predict(input_text, K=1, max_T = 300)
     return jsonify({'sql_query': output})
